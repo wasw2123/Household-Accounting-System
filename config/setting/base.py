8c017e55a57975344011c52d9583ca7635c67316
jongwonkim987 <jongwonkim987@gmail.com>
2026-04-08 13:48:37 +0900

    feat: 경로 변경 및 경로 변경으로 인한 기능들 전체적인 수정

--- config/setting/base.py
+++ config/setting/base.py
@@ -60,8 +60,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
 
