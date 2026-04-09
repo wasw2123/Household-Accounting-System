19e49d0bd112d3e40e74ab299f1cb93f7db968ce
kochanyeol <cksdufqqpu@gmail.com>
2026-04-09 13:41:45 +0900

    fix : N+1쿼리 문제 해결

--- app/transaction/views.py
+++ app/transaction/views.py
@@ -10,7 +10,14 @@ class TransactionListView(ListCreateAPIView):
     serializer_class = TransactionSerializer
 
     def get_queryset(self):
-        transaction = Transaction.objects.filter(user=self.request.user)
+        transaction = Transaction.objects.filter(user=self.request.user).select_related("account")
+        # (transaction)N -> 1(account)관계에서 select_related()로
+        # 전체를 JOIN해서 가져와서 N+1 문제를 해결
+        # 그냥 필터링으로 조회하게 돼면 쿼리 발생 시 해당 부분을 DB에서 전체를
+        # 한번씩 조회 후 해당 부분을 가져오기에 N+1문제 발생
+        # 그냥 필터링을 DB에서 조회하지않고 select_related()로 가져와서 메모리에서 찾음
+        # self.request.user을 통해 해당유저의 거래내역을 가져오고
+        # select_related()를 사용해서 계좌와 거래내역을 조인해서 가져온다
 
         transaction_type = self.request.query_params.get("type")
         amount_min = self.request.query_params.get("amount_min")
@@ -34,4 +41,4 @@ class TransactionDetailView(RetrieveUpdateDestroyAPIView):
     serializer_class = TransactionSerializer
 
     def get_queryset(self):
-        return Transaction.objects.filter(user=self.request.user)
+        return Transaction.objects.filter(user=self.request.user).select_related("account")
