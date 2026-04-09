17fe2ad62a6790208bb64ee5c144af69cc71e760
netrunner <netrunnerr25@gmail.com>
2026-04-09 10:47:39 +0900

    ✨ feat     :회원가입 기능 추가

--- app/user/views.py
+++ app/user/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from app.user.serializers import RegisterSerializer
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message" : "회원가입 완료되었습니다."}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
