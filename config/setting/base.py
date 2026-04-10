cda5abfbf91f90c6e288ded80d142483970d2486
jongwonkim987 <jongwonkim987@gmail.com>
2026-04-10 13:50:26 +0900

    feat: 보안 설정 강화 및 배포 환경 설정 추가

--- config/setting/base.py
+++ config/setting/base.py
@@ -130,6 +130,7 @@ REST_FRAMEWORK = {
         "app.user.authentication.CookieJWTAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 10,
 }
