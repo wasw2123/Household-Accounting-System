83c7ae0e48dbc5e41045d20bd683500c4eddf0bf
jongwonkim987 <jongwonkim987@gmail.com>
2026-04-10 17:32:08 +0900

    feat: 정적 파일 서빙 오류 해결

--- config/setting/base.py
+++ config/setting/base.py
@@ -28,7 +28,6 @@ DJANGO_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
