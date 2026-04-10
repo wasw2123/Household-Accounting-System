6a7951f2cb475f0f4359bf06e1f9ee2d9d9eb373
kochanyeol <cksdufqqpu@gmail.com>
2026-04-10 16:56:17 +0900

    feat : generics - ListAPIView로 views 작성

--- app/analysis/views.py
+++ app/analysis/views.py
@@ -1,1 +1,19 @@
-# Create your views here.
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Analysis
+from .serializers import AnalysisSerializer
+
+
+class AnalysisListView(ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = AnalysisSerializer
+
+    def get_queryset(self):
+        analysis = Analysis.objects.filter(user=self.request.user).select_related("user")
+
+        analysis_type = self.request.query_params.get("analysis_type")
+        if analysis_type:
+            analysis = analysis.filter(analysis_type=analysis_type)
+
+        return analysis
