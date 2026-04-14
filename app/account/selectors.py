b1699b61c4bebfafa91b2fbe593f0a466ef35c12
yimiro <s_icay@naver.com>
2026-04-14 16:44:31 +0900

    fix: settings.DEBUG 제거

--- app/account/selectors.py
+++ app/account/selectors.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.db.models import QuerySet
 
@@ -9,10 +8,7 @@ User = get_user_model()
 
 
 def get_account_list(*, user: User, account_type: str | None = None, bank_code: str | None = None) -> QuerySet[Account]:
-    if settings.DEBUG:
-        account_list = Account.objects.all().select_related("user")
-    else:
-        account_list = Account.objects.filter(user=user).select_related("user")
+    account_list = Account.objects.filter(user=user).select_related("user")
     # filter
     if account_type:
         account_list = account_list.filter(account_type=account_type)
@@ -23,8 +19,6 @@ def get_account_list(*, user: User, account_type: str | None = None, bank_code: 
 
 def get_account_detail(*, user: User, account_pk: int) -> Account:
     try:
-        if settings.DEBUG:
-            return Account.objects.select_related("user").get(pk=account_pk)
         return Account.objects.select_related("user").get(user=user, pk=account_pk)
     except Account.DoesNotExist:
         raise AccountNotFoundError("계좌를 찾을 수 없습니다.") from None
